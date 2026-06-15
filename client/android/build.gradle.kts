67b78b77baa23097569bcf3616c7e428aa08a8c5
toly <1981462002@qq.com>
2026-06-16 06:20:33 +0800

    fix: 强制所有 Android 子项目 compileSdk 36

--- client/android/build.gradle.kts
+++ client/android/build.gradle.kts
@@ -15,8 +15,17 @@ subprojects {
     val newSubprojectBuildDir: Directory = newBuildDir.dir(project.name)
     project.layout.buildDirectory.value(newSubprojectBuildDir)
 }
+
 subprojects {
     project.evaluationDependsOn(":app")
+}
+
+subprojects {
+    plugins.withType<com.android.build.gradle.BasePlugin> {
+        extensions.configure<com.android.build.gradle.BaseExtension> {
+            compileSdkVersion(36)
+        }
+    }
 }
 
 tasks.register<Delete>("clean") {
